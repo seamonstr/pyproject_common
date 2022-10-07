90c733b994f6b0472782a76c8e477bb6e701a79d
Simon Woodward <simon.woodward@zoopla.co.uk>
2022-10-07 17:20:36 +0100

    Fixes

--- setup.sh
+++ setup.sh
@@ -1,28 +1,33 @@
 set -e 
+SUBMODULE=pyproject_common
+
+[ -d $SUBMODULE ]
 
 # Copy precommit hooks
-cp pyproject_common/pre-commit .git/hooks/
+cp $SUBMODULE/pre-commit .git/hooks/
+chmod o+x .git/hooks/pre-commit
+
+# Copy other template files & commit to github	
+for i in $(ls $SUBMODULE/template); do
+	echo Copying $i
+	if cp -n $SUBMODULE/template/$i . ; then
+		git add $i
+	fi
+done
+
 # Set up python
 [ -d ".venv" ] || {
-	 python -m env .venv
+	python -m venv .venv
 	. .venv/bin/activate
 	pip install --upgrade pip
 	pip install poetry
 	poetry install
 }
 
-# Copy other template files & commit to github	
-for i in ls $SUBMODULE/template/*; do
-	if cp -n $SUBMODULE/template/$i .; then
-		git add $i
-	fi
-done
-
 # Finally, docker setup
 [ -d Docker ] || {
 	cp -R $SUBMODULE/Docker .
 	git add Docker
-
 }
 
 git commit -m "Adding template files"
