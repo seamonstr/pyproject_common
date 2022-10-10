2ea86b96a197539a49f8ac550225cf6c27fa9797
Simon Woodward <simon.woodward@zoopla.co.uk>
2022-10-10 17:44:21 +0100

    Idempotency; additional python packages.

--- setup.sh
+++ setup.sh
@@ -1,21 +1,29 @@
-set -e 
 SUBMODULE=pyproject_common
 
-[ -d $SUBMODULE ]
+if [[ ! -d $SUBMODULE ]]; then
+	echo $SUBMODULE not found.
+fi
 
-# Copy other template files & commit to github	
-cp -R $SUBMODULE/template/. .
-git add .
-git commit -m "Adding template files"
+# Copy other template files & commit to github - note -i for idempotency safety
+cp -Ri $SUBMODULE/template/. .
+if git add .; then
+  git commit -m "Adding template files"
+else
+	echo "Nothing to add."
+fi
 
-# Set up python
+# Set up python & poetry
 [ -d ".venv" ] || {
 	python -m venv .venv
-	. .venv/bin/activate
-	pip install --upgrade pip
-	pip install poetry
-	poetry install
 }
+
+. .venv/bin/activate
+pip install --upgrade pip
+pip install poetry
+poetry add black
+poetry add pyproject-flake8
+poetry add mypy
+poetry install
 
 # Copy precommit hooks last so the above doesn't bork
 cp $SUBMODULE/pre-commit .git/hooks/
